aaf4448d49bbb29f0fb4b1b2bf43a7cdb6fc974f
MatHook <mathook00@gmail.com>
2020-03-14 19:54:11 +0300

    Created 7th task ab additional to last elemnt of array.

---
+++ Arrays/№7 Plus one/plusOne.js
@@ -0,0 +1,16 @@
+const plusOne = digits => {
+  let output = [];
+  let le = digits[digits.length - 1] + 1;
+  digits.pop();
+  digits.push(le);
+  digits = parseInt(digits.toString().split(',').join(''));
+  console.log(digits);
+  while (digits > 0) {
+    output.push(digits % 10);
+    digits = parseInt(digits / 10);
+  }
+
+  return output.reverse();
+};
+
+console.log(plusOne([9]));
