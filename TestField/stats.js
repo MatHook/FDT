0fc81dcd5356971ced9919eaf5666de42c93d4a6
MatHook <mathook00@gmail.com>
2020-08-22 02:51:59 +0300

    Edited best stats

--- TestField/stats.js
+++ TestField/stats.js
@@ -1,15 +1,17 @@
 function bestStats(passes, times, desiredPercentage) {
   let tries = 0;
   while (desiredPercentage >= passes/times) {
-    console.log(`The desired percentage is ${desiredPercentage}%\n
-    passes made up ${passes}\n
-    times made up ${times}\n
-    ${passes}/${times} = ${passes/times}%\n
-    You need to win ${tries} turns`)
     passes++
     times++
     tries++
   }
+  console.log(`The desired percentage is ${desiredPercentage}%\n
+    passes made up ${passes}\n
+    times made up ${times}\n
+    ${passes}/${times} = ${passes/times}%\n
+    You need to win ${tries} turns`)
+  
+  return tries
 }
 
 console.log(bestStats(136, 160, .99));
