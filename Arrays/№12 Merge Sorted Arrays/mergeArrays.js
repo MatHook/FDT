85229cc655b442a11a8751e9c7f661aef53da91d
MatHook <mathook00@gmail.com>
2020-09-20 16:53:22 +0300

    Edited merge arrays file. Completed new task ab finding first bad version by binary search.

--- Arrays/№12 Merge Sorted Arrays/mergeArrays.js
+++ Arrays/№12 Merge Sorted Arrays/mergeArrays.js
@@ -9,11 +9,9 @@ var merge = (nums1, m, nums2, n) => {
   let [p1, p2, end] = [m-1, n-1, m+n-1]; 
     while(p1 >= 0 && p2 >= 0) {    
         nums1[end--] = nums2[p2] >= nums1[p1] ? nums2[p2--] : nums1[p1--];
-        console.log("loop1 " + nums1)
     }
     while(p2 >= 0) {
         nums1[end--] = nums2[p2--];
-        console.log("loop2 " + nums1)
     }
 };
 
