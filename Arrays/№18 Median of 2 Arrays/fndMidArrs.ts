6549b45e5ad5767182b40ada0651d9414a8e3585
MatHook <mathook00@gmail.com>
2020-10-08 14:07:04 +0300

    Get median of 2 arrays task complete

---
+++ Arrays/№18 Median of 2 Arrays/fndMidArrs.ts
@@ -0,0 +1,20 @@
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+  var output: number;
+
+  var numsRes = nums1.concat(nums2).sort((a, b) => a - b);
+
+  if(numsRes.length % 2 === 0) {  //even
+    let lMedianEl = numsRes[Math.floor(numsRes.length / 2) - 1]
+    let rMedianEl = numsRes[Math.floor(numsRes.length / 2)]
+    output = (lMedianEl + rMedianEl) / 2
+  }
+
+  if(numsRes.length % 2 === 1) {  //uneven
+    output = numsRes[Math.floor(numsRes.length / 2)]
+  }
+
+  console.log(numsRes)
+  return output;
+};
+
+findMedianSortedArrays([3], [-2, -1])
