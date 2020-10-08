8a8e4b759632481965f717e5471948507fff90bb
MatHook <mathook00@gmail.com>
2020-10-08 22:54:53 +0300

    Added new tasks.

---
+++ Arrays/№19 Sort Colors/sortClrs.ts
@@ -0,0 +1,3 @@
+function sortColors(nums: number[]): void {
+  nums = nums.sort((a, b) => a - b)
+};
---
+++ Arrays/№21 Group Anagrams/grpAngrm.js
@@ -0,0 +1,12 @@
+function groupAnagrams(strs) {
+  let obj = {};
+
+  for (let str of strs) {
+    let letters = str.split('').sort().join('')
+    obj[letters] ? obj[letters].push(str) : obj[letters] = [str];
+  }
+  
+  return Object.values(obj)
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
