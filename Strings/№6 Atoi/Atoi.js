f253426e472ba56c7794ee075645dc4c11613a3d
MatHook <mathook00@gmail.com>
2020-10-10 13:33:58 +0300

    String block completed

--- Strings/№6 Atoi/Atoi.js
+++ Strings/№6 Atoi/Atoi.js
@@ -1,5 +1,19 @@
-const atoi = (s) => {
-  
+var atoi = (s) => {
+  s = s.trim();
+  let strArr = s.split(" ");
+  let firstWord = strArr[0];
+  if (parseInt(firstWord)) {
+    let num = parseInt(firstWord);
+    if (num >= Math.pow(2, 31)) {
+      return Math.pow(2, 31) - 1;
+    } else if (num < Math.pow(-2, 31)) {
+      return Math.pow(-2, 31);
+    } else {
+      return num;
+    }
+  } else {
+    return 0;
+  }
 };
 
-console.log(atoi('42'));
+console.log(atoi("-91283472332 alksmda"));
