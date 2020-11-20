907e2d176bb38b1f08f11286e7093e6c63359ffe
MatHook <mathook00@gmail.com>
2020-11-21 00:44:51 +0300

    Fix

--- Arrays/№1 Duplicated numbers/duplicates.js
+++ Arrays/№1 Duplicated numbers/duplicates.js
@@ -1,9 +1,3 @@
-const removeDuplicates = (array) => {
-  const uniqNums = new Set(array);
-  const backToArray = [...uniqNums];
-
-  console.log(backToArray);
-  console.log(array);
-};
+const removeDuplicates = (array) => new Set([...array]);
 
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
